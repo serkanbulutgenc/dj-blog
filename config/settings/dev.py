423b87b67e3385ff2a41fd3df538c4c0c256d1bb
Serkan <serkanbulutgenc@gmail.com>
2026-02-01 17:17:58 +0300

    UPdated settings

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,5 +1,6 @@
-from .base import * #noqa
-DATABASES["default"]["NAME"] = BASE_DIR.parent/'db.sqlite3'
+from .base import *  # noqa
+
+DATABASES["default"]["NAME"] = BASE_DIR.parent / "db.sqlite3"
 
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
@@ -7,5 +8,5 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "http://localhost:5173",
-    "http://127.0.0.1:8000",
+    "http://127.0.0.1:5173",
 ]
